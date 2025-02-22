b40520f601ff977c090ebd0c8dfa8119bb1529ad
ffdossa <127089701+ffdossa@users.noreply.github.com>
2025-02-22 13:14:09 +0200

    Initial Commit

---
+++ FF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FF
+//
+//  Created by Andrii Marchuk on 22.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
